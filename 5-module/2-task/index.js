b5e35e259ad372537ed187729301ce1e1fe21068
“Maksym <maksim.riabenko@ribs.pro>
2023-01-06 03:01:59 +0200

    update second task 5 module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  document.addEventListener('click', (event) => {
+    let btn = event.target.classList.contains('toggle-text-button');
+
+    if(!btn) return;
+    
+    let element = document.querySelector('#text');
+    element.hidden = !element.hidden 
+
+  })
 }
