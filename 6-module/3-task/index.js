876a6116d9e693de7c30371447383af183121a82
“Maksym <maksim.riabenko@ribs.pro>
2023-08-27 01:43:42 +0300

    completed the 6th module, in the third task I spied the solution

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,93 +1,100 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
+
   constructor(slides) {
     this.slides = slides;
-    this.elem = this.createCarousel();
-    this.counter = 0;
-    this.transforWidth = 0;
-    this.slideWidth = 0;
-    this.carouselList = null;
-    this.rightBtn = null;
-    this.leftBtn = null; 
+
+    this.currentSlideNumber = 0;
+    this.render();
+    this.addEventListeners();
   }
 
-  createCarousel() {
-    const carousel = createElement('<div class="carousel">');
+  render() {
+    this.elem = createElement(`
+        <div class="carousel">
+          <div class="carousel__arrow carousel__arrow_right">
+            <img src="/assets/images/icons/angle-icon.svg" alt="icon" />
+          </div>
+          <div class="carousel__arrow carousel__arrow_left">
+            <img src="/assets/images/icons/angle-left-icon.svg" alt="icon" />
+          </div>
+          <div class="carousel__inner"></div>
+        </div>
+        `);
 
-    this.rightBtn = createElement(`
-    <div class="carousel__arrow carousel__arrow_right">
-      <img src="../../assets/images/icons/angle-icon.svg" alt="icon">
-    </div>`);
-    this.leftBtn = createElement(`
-    <div class="carousel__arrow carousel__arrow_left">
-      <img src="../../assets/images/icons/angle-left-icon.svg" alt="icon">
-    </div>`);
-
-    const carouselList = createElement('<div class="carousel__inner">'); 
-    const slides = this.createSlide();
-
-    slides.forEach(slide => {
-      carouselList.appendChild(slide);
-
-      setTimeout(() => {
-        this.slideWidth = slide.offsetWidth;
-      }, 0);
-    });
-
-
-    this.rightBtn.addEventListener('click', (event) => {
-      this.transforWidth -= this.slideWidth;
-      this.changeSlide('next', ++this.counter, this.transforWidth);
-    });
-
-    this.leftBtn.addEventListener('click', (event) => {
-      this.transforWidth += this.slideWidth;
-      this.changeSlide('prev', --this.counter, this.transforWidth);
-    });
-
-    carousel.appendChild(this.rightBtn);
-    carousel.appendChild(this.leftBtn);
-    carousel.appendChild(carouselList);
-
-    return carousel;
-  }
-
-  createSlide() {
-    return this.slides.map(element => {
-      return createElement(`
-      <div class="carousel__slide" data-id="${element.id}">
-        <img src="../../assets/images/carousel/${element.image}" class="carousel__img" alt="slide">
+    let slides = this.slides.map(item => createElement(`
+      <div class="carousel__slide" data-id="${item.id}">
+        <img
+          src="/assets/images/carousel/${item.image}"
+          class="carousel__img"
+          alt="slide"
+        />
         <div class="carousel__caption">
-          <span class="carousel__price">€${element.price.toFixed(2)}</span>
-          <div class="carousel__title">${element.name}</div>
+          <span class="carousel__price">€${item.price.toFixed(2)}</span>
+          <div class="carousel__title">${item.name}</div>
           <button type="button" class="carousel__button">
-            <img src="../../assets/images/icons/plus-icon.svg" alt="icon">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon" />
           </button>
         </div>
-      </div>`);
-    });
+      </div>`));
+
+    this.sub('inner').append(...slides);
+
+    this.update();
   }
 
-  changeSlide(arg, counter, transformWidth, innerCarousel) {
+  addEventListeners() {
+    this.elem.onclick = ({target}) => {
+      let button = target.closest('.carousel__button');
+      if (button) {
+        let id = target.closest('[data-id]').dataset.id;
 
-    let carouselList = document.querySelector('.carousel__inner');
+        this.elem.dispatchEvent(new CustomEvent('product-add', {
+          detail: id,
+          bubbles: true
+        }));
+      }
 
-    if (arg === 'prev') {
-      carouselList.style.transform = 'translateX(' + `${transformWidth}` + 'px)';
+      if (target.closest('.carousel__arrow_right')) {
+        this.next();
+      }
+
+      if (target.closest('.carousel__arrow_left')) {
+        this.prev();
+      }
+    };
+  }
+
+  sub(ref) {
+    return this.elem.querySelector(`.carousel__${ref}`);
+  }
+
+  next() {
+    this.currentSlideNumber++;
+    this.update();
+  }
+
+  prev() {
+    this.currentSlideNumber--;
+    this.update();
+  }
+
+  update() {
+    let offset = -this.elem.offsetWidth * this.currentSlideNumber;
+    this.sub('inner').style.transform = `translateX(${offset}px)`;
+
+    if (this.currentSlideNumber == this.slides.length - 1) {
+      this.sub('arrow_right').style.display = 'none';
+    } else {
+      this.sub('arrow_right').style.display = '';
     }
 
-    if (arg === 'next') {
-      carouselList.style.transform = 'translateX(' + `${transformWidth}` + 'px)';
+    if (this.currentSlideNumber == 0) {
+      this.sub('arrow_left').style.display = 'none';
+    } else {
+      this.sub('arrow_left').style.display = '';
     }
-
-    console.log('counter', counter);
-    console.log('this.leftBtn', this.leftBtn);
-
-
-    // counter === 0 ? this.leftBtn.style.display = 'none' : this.leftBtn.style.display = '';
-    // counter === 3 ? rightBtn.style.display = 'none' : rightBtn.style.display = '';
-
   }
 
 }
